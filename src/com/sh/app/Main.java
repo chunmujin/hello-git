632c15d53366323131aca687b394c4a87d960bd5
chunmujin <anwls0276@naver.com>
2023-11-13 11:22:16 +0900

    Main.main 대문자로 수정!

--- src/com/sh/app/Main.java
+++ src/com/sh/app/Main.java
@@ -7,8 +7,8 @@ import com.sh.app.animal.Eagle;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("hello world");
-		System.out.println("hello Git");
+		System.out.println("HELLO WORLD");
+		System.out.println("HELLO GIT");
 		
 	
 		new Dog().bark();
