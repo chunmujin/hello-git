76ad4c0a106d1b657b614ce29b6f92420470d3b0
chunmujin <anwls0276@naver.com>
2023-11-13 10:41:25 +0900

    Cat#jump 추가

--- src/com/sh/app/Main.java
+++ src/com/sh/app/Main.java
@@ -1,10 +1,18 @@
 package com.sh.app;
 
+import com.sh.app.animal.Cat;
+import com.sh.app.animal.Dog;
+
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("hello world");
 		System.out.println("hello Git");
+		
+	
+		new Dog().bark();
+		new Cat().jump();
+		
 	}
 
 }
